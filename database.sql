7e8c51acd4c7199424ba9a1a99395a78a6072a06
Luke Schlangen <lukeschlangen@gmail.com>
2016-11-28 10:17:20 -0600

    typo fix

--- database.sql
+++ database.sql
@@ -10,7 +10,7 @@ INSERT INTO super_powers (name, description)
 VALUES ('Invisibility', 'Subject is able to become completely unseen to the unaided eye. Usually remains detectible with infrared sensors as well as auditory.'),
 ('Flight', 'Subject can defy gravity at will, requiring no obvious means of propulsion.'),
 ('Super Speed', 'Subject can move at high speeds. This can be running, swimming, or any other non-flight locomotion.'),
-('Super Strength', 'Subject posseses physicl strength well beyond the normal human range. Lift maximum is often measured in tons, not pounds.'),
+('Super Strength', 'Subject posseses physical strength well beyond the normal human range. Lift maximum is often measured in tons, not pounds.'),
 ('Accelerated Healing', 'Subject is able to heal physical wounds or illnesses at a rapid pace'),
 ('Power Blast', 'General. Subject can emit a dangerous energy from various parts of their body. Energy types vary.'),
 ('Animal Affinity', 'Subject can communicate with various animals. Sometimes this is limited to specific types of animals. ie: rodents, fish, etc.'),
