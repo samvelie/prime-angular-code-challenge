1a211e27d9e458301937cb9132272b2e8abae008
samvelie <sam.velie@gmail.com>
2017-03-13 14:04:42 -0500

    adding hero with dropdown using ids from superpower table

--- server/public/scripts/controllers/hero-entry.js
+++ server/public/scripts/controllers/hero-entry.js
@@ -1,4 +1,38 @@
 app.controller('AddHeroController', ['$http', function($http){
   var self = this;
-  
+
+  self.powersArray = []; //this gets filled in by the getSuperPowersList function
+
+  getSuperPowersList();
+
+
+  self.newHero = { //this object is linked to the inputs
+    persona: '',
+    alias: '',
+    power_id: 0
+  };
+
+  self.addHero = function(){
+
+    console.log("addHero clicked");
+    var heroObject = self.newHero;
+    console.log(heroObject);
+    $http({
+      method: 'POST',
+      url: '/heroes',
+      data: heroObject
+    }).then(function(response){
+        console.log(response);
+    });
+  };
+
+  function getSuperPowersList(){
+    $http({
+      method: 'GET',
+      url: '/powers'
+    }).then(function(response){
+      console.log(response.data);
+      self.powersArray = response.data;
+    });
+  }
 }]);
