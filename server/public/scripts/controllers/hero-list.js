e6a4f28b5ccb45fe04e3e0475f2c9ca3e527a462
samvelie <sam.velie@gmail.com>
2017-03-13 13:24:43 -0500

    get ajax and displaying in table working

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -1,3 +1,17 @@
-app.controller('HeroListController', function(){
+app.controller('HeroListController', ['$http', function($http){
     console.log('Hero List Controller loaded');
-});
+    var self = this;
+
+    getHeroes();
+    self.HeroesArray = [];
+
+    function getHeroes(){
+      $http({
+        method: 'GET',
+        url: '/heroes'
+      }).then(function(response){
+        console.log(response.data);
+        self.HeroesArray = response.data;
+      });
+    }
+}]);
