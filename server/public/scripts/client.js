fdf814f1d806c65f01582e40d96799d05b3ddd79
samvelie <sam.velie@gmail.com>
2017-03-13 13:33:47 -0500

    2nd view added with controller, sourcing in

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,7 +7,12 @@ app.config(['$routeProvider', function($routeProvider) {
       controller: 'HeroListController',
       controllerAs: 'hlc'
     })
+    .when('/addHero', {
+      templateUrl: '/views/hero-entry.html',
+      controller: 'AddHeroController',
+      controllerAs: 'ah'
+    })
     .otherwise({
       redirectTo: 'heroList'
-    })
+    });
 }]);
---
+++ server/public/scripts/controllers/hero-entry.js
@@ -0,0 +1,4 @@
+app.controller('AddHeroController', ['$http', function($http){
+  var self = this;
+  
+}]);
